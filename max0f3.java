7c934ef0eebf6bdc94ca0af530eac5f23be0908c
Swetha-2508 <163889342+Swetha-2508@users.noreply.github.com>
2024-11-05 20:17:40 +0530

    Create max0f3.java

---
+++ max0f3.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+class Hlo
+{
+    public static void main(String args[])
+    {
+    Scanner sc=new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int c = sc.nextInt();
+    if (a==b && b==c)
+    {
+        System.out.println("all are equal");
+    }
+    else
+    {
+        if(a>b)
+        {
+            if(a>c)
+            {
+                System.out.println("max is a");
+            }
+            else
+            {
+                System.out.println("max is c");
+            }
+        }
+        else
+        {
+            if(b>c)
+            {
+                System.out.println("max is b");
+            }
+            else
+            {
+                System.out.println("max is c");
+            }
+        }
+    }
+    }
+}
