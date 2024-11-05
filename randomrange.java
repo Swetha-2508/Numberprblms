0f479cdc6d93eb2dbaf8493d89d314993765acca
Swetha-2508 <163889342+Swetha-2508@users.noreply.github.com>
2024-11-05 21:30:33 +0530

    Create randomrange.java

---
+++ randomrange.java
@@ -0,0 +1,10 @@
+public class randomrange {
+    public static void main(String args[])
+    {
+        int minbound=10;
+        int maxbound=20;
+        int random=(int)(Math.random()*(maxbound-minbound+1))+minbound;// 20-10+1=11 maxbound inclusive(size of range) , * fit the random value in range ,+minbound changes the range from 0tomaxbound to minbound to maxbound
+        System.out.println(random);//Math.random() results random double value
+
+    }
+}
