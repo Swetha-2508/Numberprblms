c279c5a5dd7d5ad67a9f0b2ec3c69033739221e4
Swetha-2508 <163889342+Swetha-2508@users.noreply.github.com>
2024-11-05 21:11:20 +0530

    Create factorial.java

---
+++ factorial.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class factorial
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=1;
+        while(n==1)
+        {
+            System.out.println("Enter a number");
+            int num=sc.nextInt();
+            int factorial=1; //initialising factorial variable before loop (updated factorial value will be stored)
+            for(int i=1;i<=num;i++)
+            {
+                factorial=factorial*i;
+            }
+            System.out.println(factorial);
+            System.out.println("Need to continue for next number.Enter one");
+            n=sc.nextInt();
+        }
+    }
+}
