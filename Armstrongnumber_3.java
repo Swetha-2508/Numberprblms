0c336fda1773c0bc354064999a54abbe0a097dc0
Swetha <163889342+Swetha-2508@users.noreply.github.com>
2025-01-08 17:49:01 +0530

    Update and rename Armstrongnumber.java to Armstrongnumber_3.java

--- Armstrongnumber_3.java
+++ Armstrongnumber_3.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Armstrongnumber {
+public class Armstrongnumber_3 {
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);        
