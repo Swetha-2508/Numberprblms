08da2fb47b03d3ef2b7cd16d44a016b2a7b53dde
Swetha <163889342+Swetha-2508@users.noreply.github.com>
2025-01-23 16:45:29 +0530

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 class Fibonacci
 {
     public static void main(String[] args) {
@@ -5,7 +6,7 @@ class Fibonacci
         System.out.println("Enter the series count of fibonacci");
         int count=sc.nextInt();
         int n1=0,n2=1,n3;
-        System.out.print(n1+" "+n2+" ");
+        System.out.print(n2+" ");
         for(int i=2;i<count;i++)
         {
             n3=n1+n2;
