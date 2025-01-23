de9276577ff8ce49ab8d83c78ccce1f77d3b47ca
Swetha <163889342+Swetha-2508@users.noreply.github.com>
2025-01-23 21:39:55 +0530

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -7,7 +7,7 @@ class Fibonacci
         int count=sc.nextInt();
         int n1=0,n2=1,n3;
         System.out.print(n2+" ");
-        for(int i=2;i<count;i++)
+        for(int i=2;i<=count;i++)
         {
             n3=n1+n2;
             System.out.print(n3+" ");
